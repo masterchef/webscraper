fb5eb7f2ba5312fca98891628521e48dba024d72
Viktoras Truchanovicius <vtruchanovicius@linkedin.com>
2020-09-05 14:05:34 -0700

    Adding ability to save to gsheets

--- scrapper.py
+++ scrapper.py
@@ -1,5 +1,6 @@
 import datetime
 import gspread
+import getpass
 import pandas as pd
 import re
 import smtplib
@@ -23,23 +24,36 @@ DOC_KEY = '1XZocxmyQ91e1exBvwDAaSR8Rhavy9WPnwLSz0Z5SKsM'
 @click.option('--email', is_flag=True, help='A flag for sending email with results.')
 @click.option('--email_to', help='CSV of email addresses to send notification to.')
 @click.option('--username', help='SMTP account username.')
-@click.option('--password', prompt='Enter your password for email server', hide_input=True, help='SMTP account password.')
-def cli(email, email_to, username, password):
+@click.option('--gsheet', is_flag=True, help='A flag for updating google sheet with results')
+@click.option('--doc_key', help='Google Doc Key to update')
+@click.pass_context
+def cli(ctx, email, email_to, username, gsheet, doc_key):
+    ctx.ensure_object(dict)        
     if email and (not username or not email_to):
         print('Please provide email sending parameters')
+        exit(0)
+    elif email:
+        password = getpass.getpass("Please enter your google account password for sending email:\n")
+        ctx.obj['password'] = password
+    if gsheet and not doc_key:
+        print('Please provide a gsheet doc key')
         exit(0)
     pass
 
 
 @cli.command('schedule')
+@click.option('--hour', default='*/1', help='Cron hour expression')
 @click.pass_context
-def schedule(ctx):
+def schedule(ctx, hour):
+    email = ctx.parent.params['email']
     username = ctx.parent.params['username']
     email_to = ctx.parent.params['email_to']
-    password = ctx.parent.params['password']
-    
+    password = ctx.obj.get('password', None)
+    gsheet = ctx.parent.params['gsheet']
+    doc_key = ctx.parent.params['doc_key']
+
     schedule = BlockingScheduler()
-    schedule.add_job(run, kwargs={"email": True, "username": username, "email_to": email_to, "password": password}, trigger='cron', hour='*/1')
+    schedule.add_job(run, kwargs={"email": email, "gsheet": gsheet, "doc_key": doc_key, "username": username, "email_to": email_to, "password": password}, trigger='cron', hour=hour)
     try:
         schedule.start()
     except (KeyboardInterrupt, SystemExit):
@@ -50,38 +64,57 @@ def schedule(ctx):
 @click.pass_context
 def once(ctx):
     email = ctx.parent.params['email']
+    gsheet = ctx.parent.params['gsheet']
     username = ctx.parent.params['username']
     email_to = ctx.parent.params['email_to']
-    password = ctx.parent.params['password']
-    run(email, username, email_to, password)
+    password = ctx.obj.get('password', None)
+    doc_key = ctx.parent.params['doc_key']
+    run(email, username, email_to, password, gsheet, doc_key)
 
 
-def run(email, username, email_to, password):
-    apartments = [
-        {'name': 'Mansion Grove', 'url': 'https://prometheusapartments.com/ca/santa-clara-apartments/mansion-grove/lease/'},
-        # {'name': 'Shadowbrook', 'url': 'https://prometheusapartments.com/ca/sunnyvale-apartments/shadowbrook/lease/'}
-    ]
-
+def run(email, username, email_to, password, gsheet, doc_key):
     content = {}
-    for apt in apartments:
-        content[apt['name']] = get_availability(apt['url'])
+    content.update(get_prometheus_apartments('https://prometheusapartments.com/search/?term=San+Francisco+Bay+Area'))
+    content.update(get_prometheus_apartments('https://prometheusapartments.com/search/?term=Portland'))
+    content.update(get_prometheus_apartments('https://prometheusapartments.com/wa/gig-harbor-apartments/cliffside/'))
 
     formatted_content = format_email(content)
     if email:
-        send_email(username, password, email_to, formatted_content)
+        send_email(username, password, email_to, [content for content in formatted_content if  'mansion-grove' in content.keys()])
     
-    update_historical_data(content)
+    if gsheet:
+        update_historical_data(doc_key, content)
     print(formatted_content)
 
 
-def update_historical_data(content):
+def get_prometheus_apartments(url):
+    content = {}
+    driver = get_browser()
+    driver.get(url)
+    try:
+        anchors = driver.find_elements_by_xpath("//div[@id='results-cards']/div/a[@class='card-wrapper']")
+    except Exception as e:
+        print(f'{e}')
+        return content
+
+    links = [a.get_attribute('href') for a in anchors]
+    apartments = []
+    for apt in links:
+        name = apt.strip('/').split('/')[-1]
+        apartments.append({'name': name, 'url': f'{apt}lease'})
+    for apt in apartments:
+        content[apt['name']] = get_availability(apt['url'])
+    return content
+
+
+def update_historical_data(doc_key, content):
     date = datetime.datetime.today().strftime('%Y-%m-%d')
     all_content = []
     for key, data in content.items():
         for row in data:
             cleaned_values = [f'{date}', f'{key}'] + [value.replace('$', '').replace(',', '') for value in row]
             all_content.append(cleaned_values)
-    update_gdoc(all_content)
+    update_gdoc(doc_key, all_content)
 
 
 def format_email(content):
@@ -108,28 +141,32 @@ def get_browser():
 def get_availability(url):
     driver = get_browser()
     driver.get(url)
-
+    content = []
+    print(f'Processing {url}')
     delay = 30 # seconds
     try:
         WebDriverWait(driver, delay).until(EC.frame_to_be_available_and_switch_to_it('rp-leasing-widget'))
-        print('iframe loaded')
         WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, "//button[contains(@class, 'primary')][contains(text(), 'Start')]")))
     except TimeoutException:
         print('Waiting for iframe')
 
     # import pdb; pdb.set_trace()
-    driver.find_element_by_xpath("//button[contains(@class, 'primary')][contains(text(), 'Start')]").click()
-    WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, "//a[contains(@class, 'ng-binding')]")))
-    driver.find_element_by_xpath("//a[contains(@class, 'ng-binding')]").click()
+    try:
+        driver.find_element_by_xpath("//button[contains(@class, 'primary')][contains(text(), 'Start')]").click()
+        WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, "//a[contains(@class, 'ng-binding')]")))
+        driver.find_element_by_xpath("//a[contains(@class, 'ng-binding')]").click()
 
 
-    # Print plan prices
-    names = driver.find_elements_by_xpath("//div[contains(@class, 'floorplan-tile')]/div/span[contains(@class, 'name')]")
-    specs = driver.find_elements_by_xpath("//div[contains(@class, 'floorplan-tile')]/div/span[contains(@class, 'specs')]")
-    prices = driver.find_elements_by_xpath("//div[contains(@class, 'floorplan-tile')]/div/span[contains(@class, 'range')]")
-    availability = driver.find_elements_by_xpath("//div[contains(@class, 'floorplan-tile')]/div[@class='tile-buttons']/button")
+        # Print plan prices
+        names = driver.find_elements_by_xpath("//div[contains(@class, 'floorplan-tile')]/div/span[contains(@class, 'name')]")
+        specs = driver.find_elements_by_xpath("//div[contains(@class, 'floorplan-tile')]/div/span[contains(@class, 'specs')]")
+        prices = driver.find_elements_by_xpath("//div[contains(@class, 'floorplan-tile')]/div/span[contains(@class, 'range')]")
+        availability = driver.find_elements_by_xpath("//div[contains(@class, 'floorplan-tile')]/div[@class='tile-buttons']/button")
+    except Exception as e:
+        print(f'{e}')
+        print(f'Unable to parse {url}')
+        return content
 
-    content = []
     for i in range(len(names)):
         match = re.match(r'\((\d+)\).*', availability[i].text)
         units = 0
@@ -155,7 +192,7 @@ def send_email(username, password, to, content):
     s.quit()
 
 
-def update_gdoc(cells):
+def update_gdoc(doc_key, cells):
     scope = [
         "https://spreadsheets.google.com/feeds",
         "https://www.googleapis.com/auth/drive",
@@ -166,14 +203,14 @@ def update_gdoc(cells):
     )
 
     docs = gspread.authorize(credentials)
-    sheet = docs.open_by_key(DOC_KEY).sheet1
+    sheet = docs.open_by_key(doc_key).sheet1
     new = pd.DataFrame(cells)
     new.columns = ['Date', 'Complex', 'Plan', 'Specs', 'Price', 'Availability']
     existing = pd.DataFrame(sheet.get_all_values()[1:])
     if existing.size:
         existing.columns = ['Date', 'Complex', 'Plan', 'Specs', 'Price', 'Availability']
     updated = existing.append(new)
-    updated = updated.groupby(['Date', 'Complex', 'Plan', 'Specs']).max()
+    updated = updated.groupby(['Date', 'Complex', 'Plan', 'Specs']).min()
     updated.reset_index(inplace=True)
     sheet.update([updated.columns.values.tolist()] + updated.values.tolist(), value_input_option='USER_ENTERED')
 
